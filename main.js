6144abead69e9bbe1ce4b83fd9bd01944fedc789
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-04 15:28:04 -0500

    push

--- main.js
+++ main.js
@@ -3,42 +3,167 @@ var player1 = null;
 var player2 = null;
 var move = null;
 var gameOver = false;
+var x = null;
+var y = null;
 var board = [
-				[' ',' ',' '],
-				[' ',' ',' '],
-				[' ',' ',' ']
-			]; 
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
 var gameBoard = function() {
-		console.log(
-	'   1   '+'   2   '+'   3   \n'+
-	'~~~~~~~~~~~~~~~~~~~~~~~\n'+
-	'1| '+board[0][0]+'   |    '+board[0][1]+'  |   '+board[0][2]+'   |\n'+
-	'~~~~~~~~~~~~~~~~~~~~~~~\n'+
-	'2| '+board[1][0]+'   |    '+board[1][1]+'  |   '+board[1][2]+'   |\n'+
-	'~~~~~~~~~~~~~~~~~~~~~~~\n'+
-	'3| '+board[2][0]+'   |    '+board[2][1]+'  |   '+board[2][2]+'   |\n');
-	};
+    console.log(
+        '   1   ' + '   2   ' + '   3   \n' +
+        '~~~~~~~~~~~~~~~~~~~~~~~\n' +
+        '1| ' + board[0][0] + '   |    ' + board[0][1] + '  |   ' + board[0][2] + '   |\n' +
+        '~~~~~~~~~~~~~~~~~~~~~~~\n' +
+        '2| ' + board[1][0] + '   |    ' + board[1][1] + '  |   ' + board[1][2] + '   |\n' +
+        '~~~~~~~~~~~~~~~~~~~~~~~\n' +
+        '3| ' + board[2][0] + '   |    ' + board[2][1] + '  |   ' + board[2][2] + '   |\n');
+};
+
+
+var userOne = function() {
+    console.log(player1 + ', you are X\'s and it\'s your turn');
+    move = prompt().split(' ');
+    x = parseInt(move[0]);
+    y = parseInt(move[1]);
+    while (x > 3 || x < 1 || y > 3 || y < 1) {
+        console.log('You must enter numbers between 1 and 3 that are seperated by a space.');
+        userOne();
+    };
+    while (board[x - 1][y - 1] === ('X' || 'O')) {
+        console.log('That space is already taken, please choose again.');
+        userOne();
+    };
+    board[x - 1][y - 1] = 'X';
+    gameBoard();
+    var rowOne = [board[0][0], board[0][1], board[0][2]];
+    var rowTwo = [board[1][0], board[1][1], board[1][2]];
+    var rowThree = [board[2][0], board[2][1], board[2][2]];
+    var colOne = [board[0][0], board[1][0], board[2][0]];
+    var colTwo = [board[0][1], board[1][1], board[2][1]];
+    var colThree = [board[0][2], board[1][2], board[2][2]];
+    var diagOne = [board[0][0], board[1][1], board[2][2]];
+    var diagTwo = [board[0][2], board[1][1], board[2][0]];
+    if (rowOne.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (rowOne.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (rowTwo.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (rowThree.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (colOne.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (colTwo.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (diagOne.toString() === 'X,X,X') {
+        gameOver = true;
+    } else if (diagTwo.toString() === 'X,X,X') {
+        gameOver = true;
+    };
+    
+    console.log(board);
+    console.log(rowOne.toString());
+    userTwo();
+};
+var userTwo = function() {
+    console.log(player2 + ', you are O\'s and it\'s your turn');
+    move = prompt().split(' ');
+    x = parseInt(move[0]);
+    y = parseInt(move[1]);
+    while (x > 3 || x < 1 || y > 3 || y < 1) {
+        console.log('You must enter numbers between 1 and 3 that are seperated by a space.');
+        userTwo();
+    };
+    while (board[x - 1][y - 1] === ('X' || 'O')) {
+        console.log('That space is already taken, please choose again.');
+        userTwo();
+    };
+    board[x - 1][y - 1] = 'O';
+    var rowOne = [board[0][0], board[0][1], board[0][2]];
+    var rowTwo = [board[1][0], board[1][1], board[1][2]];
+    var rowThree = [board[2][0], board[2][1], board[2][2]];
+    var colOne = [board[0][0], board[1][0], board[2][0]];
+    var colTwo = [board[0][1], board[1][1], board[2][1]];
+    var colThree = [board[0][2], board[1][2], board[2][2]];
+    var diagOne = [board[0][0], board[1][1], board[2][2]];
+    var diagTwo = [board[0][2], board[1][1], board[2][0]];
+    if (rowOne.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (rowTwo.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (rowThree.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (colOne.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (colTwo.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (colThree.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (diagOne.toString() === 'O,O,O') {
+        gameOver = true;
+    } else if (diagTwo.toString() === 'O,O,O') {
+        gameOver = true;
+    };
+    gameBoard();
+    userOne();
+};
+
 
 console.log('Lets play Tic Tac Toe!');
 console.log('You\'re player one, what\'s your name?');
-     player1 = prompt();
+player1 = prompt();
 console.log('OK, now who is player 2?');
-     player2 = prompt();
+player2 = prompt();
 gameBoard();
-while (gameOver === false) {
-		console.log(player1+', it\'s your turn!');
-		move = prompt().split(' ');
-		var x = parseInt(move[0]) - 1;
-		var y = parseInt(move[1]) - 1;
-		
-		board[x][y] = 'X';
-		gameBoard();
-		gameOver = true;
-}
-
-	
+userOne();
 
 
+// loop to check if the input is valid
+// while (x > 3 || x < 1 || y >3 || y <1) {
+// 		console.log('You must enter numbers between 1 and 3 that are seperated by a space.');
+// 		gameBoard();
+// 		move = prompt().split(' ');
+// 		var x = parseInt(move[0]);
+// 		var y = parseInt(move[1]);
+// 		};
 
+// // loop to check if the space is available
+// while (board[x-1][y-1] = ('X' || 'O')) {
+// 		console.log('That space is already taken, please choose again.');
+// 		gameBoard();
+// 		move = prompt().split(' ');
+// 		var x = parseInt(move[0]);
+// 		var y = parseInt(move[1]);
+// };
+// 		//prints to board 
+// 		board[x-1][y-1] = 'X';
+// 		gameBoard();
+// 		gameOver = true;
 
-
+// // checks to see if player has won
+// if (rowOne === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 		gameOver = true;
+// }
+// else if (rowTwo === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (rowThree === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (colOne === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (colTwo === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (colThree === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (diagOne === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
+// else if (diagTwo === (['X','X','X'] || ['O', 'O', 'O'])) {
+// 	gameOver = true;
+// }
